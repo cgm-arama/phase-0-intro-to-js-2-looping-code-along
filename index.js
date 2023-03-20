e29c637980f0f2a5a8e8009eeca7827040a9205e
Eric <eric.arama@student.moringaschool.com>
2023-03-20 10:16:01 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+function countDown(number) {
+    for (let x = number; x >= 0; x--) {
+      console.log(x);
+    }
+  }
+
+  countDown(10);
+
+  function writeCards(names, event) {
+    let messages = [];
+    for (let x = 0; x < names.length; x++) {
+      messages.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+
+  let names = ['Eric', 'Terrence', 'Steve'];
+let event = 'birthday';
+
+let messages = writeCards(names, event);
+
+console.log(messages);
+
+  
